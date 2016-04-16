c8fc52a7a46e4980d5c4adcaee1009ae3590ac85
jerett <jerett.sierad@cs.tufts.edu>
2016-04-16 19:14:18 -0400

    conductor testing to be done

--- conductor.py
+++ conductor.py
@@ -1,11 +1,13 @@
 import socket
+import select
 import sys
 import thread
 import mido
 import OSC
 import glob
+import time
 
-import Adafruit_CharLCD as LCD
+#import Adafruit_CharLCD as LCD
 
 
 NEWPATCH = 3
@@ -14,16 +16,12 @@ TYPELENGTH = 8
 
 DEFAULTSYNTH = "synth1"
 MIDIDEVICE = "MPKmini2 MIDI 1"
+DEFAULTPATCH = "synth1"
 
 MIDI_ON = "note_on"
 MIDI_OFF = "note_off"
 MIDI_PARAM = "control_change"
 MIDI_WHEEL = "pitchwheel"
-
-# Initialize the LCD using the pins
-lcd = LCD.Adafruit_CharLCDPlate()
-keyb = mido.open_input(MIDIDEVICE)
-synthesizers = getAllPatches()
 
 
 class VoiceManager():
@@ -66,10 +64,13 @@ class VoiceManager():
 class Conductor():
     def __init__(self):
         self.voiceManagers = []
-        self.lcd = LCD.Adafruit_CharLCDPlate()
-        self.keyb = mido.open_input(MIDIDEVICE)
+        #self.lcd = LCD.Adafruit_CharLCDPlate()
+        #self.keyb = mido.open_input(MIDIDEVICE)
         self.synthesizers = self.getAllPatches()
-        self.midiToOSC = self.loadMidiToOSC()
+        self.midiToOSC = self.loadMidiToOSC(DEFAULTPATCH)
+        self.defaults = self.loadDefaults(DEFAULTPATCH)
+        self.currentPatch = DEFAULTPATCH
+        self.LCDcounter = 0
 
     ##simple implementation of round robbin could be optimized to use ring buffer instead
     def play(self, note, velocity):
@@ -86,6 +87,10 @@ class Conductor():
     def load(self, patch):
         for vm in self.voiceManagers:
             vm.load(patch)
+        sleep(1)
+        self.midiToOSC = self.loadMidiToOSC(patch)
+        self.loadDefaults(patch)
+        self.setDefaultsAll()
 
     def control(self, cc, value):
         path = self.midiToOSC[cc]
@@ -93,7 +98,7 @@ class Conductor():
             vm.control(value, path)
 
     def loadMidiToOSC(self, patch):
-        file = open(patch + "mtoosc", 'r')
+        file = open("./Patches/" + patch + ".properties", 'r')
         midiToOSC = {}
         for line in file:
             line = line.split()
@@ -101,12 +106,27 @@ class Conductor():
         return midiToOSC
 
 
-    def setDefaults(self, patch):
-        file = open(patch + "defaults", 'r')
+    def loadDefaults(self, patch):
+        file = open("./Patches/" + patch +".defaults", 'r')
+        defaults = {}
         for line in file:
             line = line.split()
-            for vm in self.voiceManagers:
-                vm.control(line[1], line[0])
+            defaults[line[0]] = line[1]
+        return defaults
+
+    def setDefaultsAll(self):
+        for path,value in self.defaults.iteritems():
+            for voiceManager in self.VoiceManagers:
+                voiceManager.control(value,path)
+
+
+    def setDefaultsOne(self, voiceManager):
+        for path,value in self.defaults.iteritems():
+            voiceManager.control(value,path)
+
+    def loadSynthesizer(self, patch):
+        self.load(patch)
+
 
     def getAllPatches(self):
         synths = []
@@ -116,34 +136,39 @@ class Conductor():
         synths.remove("voiceMaster")
         return synths
 
-    @static_var("counter", 0)
     def LCDchecker():
         if lcd.is_pressed(LCD.SELECT):
+            print "SELECT"
             lcd.clear()
-            name = synthesizers[LCDchecker.counter].name
+            name = self.synthesizers[self.LCDcounter]
             lcd.message("[" + name + "]")
-            loadSynthesizer(name)
-        elif lcd.is_pressed(LCD.LEFT):
-            lcd.clear()
-        elif lcd.is_pressed(LCD.RIGHT):
-            lcd.clear()
+            self.loadSynthesizer(name)
+        # elif lcd.is_pressed(LCD.LEFT):
+        #     lcd.clear()
+        # elif lcd.is_pressed(LCD.RIGHT):
+        #     lcd.clear()
         elif lcd.is_pressed(LCD.UP):
+            print "UP"
             lcd.clear()
-            LCDchecker.counter = max(LCDchecker.counter - 1, 0)
-            lcd.message(synthesizers[LCDchecker.counter].name)
+            self.LCDcounter = max(self.LCDcounter - 1, 0)
+            lcd.message(self.synthesizers[self.LCDcounter])
         elif lcd.is_pressed(LCD.DOWN):
+            print "DOWN"
             lcd.clear()
-            LCDchecker.counter = min(LCDchecker.counter + 1, len(synthesizers))
-            lcd.message(synthesizers[LCDchecker.counter].name)
+            self.LCDcounter = min(self.LCDcounter + 1, len(self.synthesizers))
+            lcd.message(self.synthesizers[self.LCDcounter])
 
     # This needs to parse message correctly
     def midiCheck():
         for message in self.keyb:
             if message.type == MIDI_ON:
+                print "ON"
                 self.play(message.note, message.velocity)
             elif message.type == MIDI_OFF:
+                print "OFF"
                 self.stop(message.note)
             elif message.type == MIDI_PARAM:
+                print "PARAM"
                 self.control(message.cc, message.value)
 
     def mainLoop(self):
@@ -163,10 +188,15 @@ class Conductor():
                     if(messageType == READY):
                         maxVoices = int(s.recv(intLength))
                         vMPort = s.recv(ipLength)
-                        voiceManagers.append(VoiceManager(
-                            maxVoices, client_address, vMPort))
+                        newVM = VoiceManager( maxVoices, client_address, vMPort)
+                        newVM.load(self.currentPatch)
+                        self.setDefaultsOne(newVM)
+                        voiceManagers.append(newVM)
                         inputs.append(connection)
 
-c = Conductor()
-c.mainLoop()
 
+c = Conductor()
+c.load
+print c.defaults
+print c.midiToOSC
+c.mainLoop()
